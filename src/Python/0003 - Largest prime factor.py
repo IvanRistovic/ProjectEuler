6d31baf39a0cebf2f5da51be0398f7b7811f567b
IvanRistovic <ivo.ristovic@gmail.com>
2016-11-20 16:04:03 +0100

    Add files via upload

---
+++ src/Python/0003 - Largest prime factor.py
@@ -0,0 +1,22 @@
+ # Problem 3
+ # The prime factors of 13195 are 5, 7, 13 and 29.
+ #
+ # What is the largest prime factor of the number 600851475143 ?
+
+number = 600851475143
+largest_pf = 1
+
+i = 2
+while i <= number:
+	while number % i == 0:
+		number /= i
+
+	if i > largest_pf:
+		largest_pf = i
+
+	i += 1
+
+print largest_pf
+
+ # Solution:
+ # 6857
---
+++ src/Python/0004 - Largest palindrome product.py
@@ -0,0 +1,29 @@
+ # Problem 4
+ # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+ #
+ # Find the largest palindrome made from the product of two 3-digit numbers.
+
+def is_palindrome(n):
+	reverse = 0
+	number = n
+
+	while number > 0:
+		reverse = reverse * 10 + number % 10
+		number /= 10
+
+	if n == reverse:
+		return True
+	else:
+		return False
+
+max_palindrome = 0
+for i in range(999, 100, -1):
+	for j in range(999, 100, -1):
+		current = i*j
+		if current > max_palindrome and is_palindrome(current):
+			max_palindrome = current
+
+print max_palindrome
+
+ # Solution:
+ # 906609
