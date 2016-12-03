0bb334b2c9aa63e87de5e1f417ba643690435a95
IvanRistovic <ivo.ristovic@gmail.com>
2016-12-03 15:34:57 +0100

    Added files via upload

--- src/C/0013 - Large sum.c
+++ src/C/0013 - Large sum.c
@@ -10,29 +10,29 @@
 #include "largenum.h"
 
 int main() {
-    FILE *f = fopen("0013.txt", "r");
-    largenum sum, temp;
-    char s[LARGENUM_MAX_DIGITS];
-    
-    if (f == NULL)
-        exit(EXIT_FAILURE);
-    
-    sum = load("0");
-    while (fscanf(f, "%s", s) == 1) {
-        temp = load(s);
-        sum = add(sum, temp);
-    }
-    fclose(f);
-    
-    printnum(sum);
-    
-    return 0;
+	FILE *f = fopen("0013.txt", "r");
+	largenum sum, temp;
+	char s[LARGENUM_MAX_DIGITS];
+
+	if (f == NULL)
+		exit(EXIT_FAILURE);
+
+	sum = load("0");
+	while (fscanf(f, "%s", s) == 1) {
+		temp = load(s);
+		sum = add(sum, temp);
+	}
+	fclose(f);
+
+	printnum(sum);
+
+	return 0;
 }
 
 /*
  Solution:
  5537376230390876637302048746832985971773659831892672
- 
+
  real 0m0.004s
  user 0m0.000s
  sys  0m0.000s
---
+++ src/Python/0013 - Large sum.py
@@ -0,0 +1,18 @@
+# Problem 13
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers
+# (see 0013.txt)
+
+try:
+	with open("0013.txt", "r") as f:
+		lines = f.readlines();
+except IOError:
+	exit("error: fopen error")
+
+sum = 0
+for line in lines:
+	sum += int(line)
+
+print sum
+
+# Solution:
+# 5537376230390876637302048746832985971773659831892672
