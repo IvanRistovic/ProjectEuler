7faa2d1b8ab98de2ad5efa9e3dfa0165bb29ba85
ivan-ristovic <ivo.ristovic@gmail.com>
2017-07-30 09:28:10 -0700

    0023 opt

--- src/0023 - Non-abundant sums/main.cpp
+++ src/0023 - Non-abundant sums/main.cpp
@@ -20,33 +20,38 @@
 	of two abundant numbers.
 */
 
+/*
+	Some things that might help:
+	1. Every multiple of a perfect number is abundant.
+	2. Every multiple of an abundant number is abundant.
+	3. Every integer greater than 20161 can be written as the sum of two abundant numbers.
+*/
+
 #include <iostream>
 #include <vector>
+#include <cstring>
 #include "../divisors.h"
 #include "../timer.h"
 
-#define START 12
-#define LIMIT 28123
+#define LIMIT 20161
 
-void create_sums(std::vector<unsigned> &abundant_numbers, std::vector<bool> &sums);
+void create_abundants(std::vector<unsigned> &abundant_numbers, bool abund_ind[]);
+void create_sums(std::vector<unsigned> &abundant_numbers, bool abund_ind[]);
 
 
 int main()
 {
 	ProgramTimer timer;
 
+	bool abund_ind[LIMIT + 1];
 	std::vector<unsigned> abundant_numbers;
-	std::vector<bool> sums(LIMIT + 1, false);
 
-	for (unsigned i = START; i < LIMIT; i++)
-		if (pe_div::div_sum(i) > i)
-			abundant_numbers.push_back(i);
-
-	create_sums(abundant_numbers, sums);
+	create_abundants(abundant_numbers, abund_ind);
+	create_sums(abundant_numbers, abund_ind);
 
 	unsigned sum = 0;
 	for (unsigned i = 1; i <= LIMIT; i++)
-		if (!sums[i])
+		if (!abund_ind[i])
 			sum += i;
 
 	std::cout << sum << '\n';
@@ -56,13 +61,44 @@ int main()
 }
 
 
-void create_sums(std::vector<unsigned> &abundant_numbers, std::vector<bool> &sums)
+void create_abundants(std::vector<unsigned> &abundant_numbers, bool abund_ind[])
 {
+	// Reset indicators
+	std::memset(abund_ind, false, LIMIT);
+
+	for (unsigned i = 2; i < LIMIT; i++) {
+		if (!abund_ind[i]) {
+			unsigned divsum = pe_div::div_sum(i);
+
+			// If the number is perfect
+			if (divsum == i) {
+				for (unsigned j = 2; j * i <= LIMIT; j++)
+					abund_ind[j * i] = true;
+			}
+
+			// If the number is abundant
+			if (divsum > i) {
+				for (unsigned j = 1; j * i <= LIMIT; j++)
+					abund_ind[j * i] = true;
+			}
+		}
+	}
+
+	for (unsigned i = 0; i < LIMIT; i++)
+		if (abund_ind[i])
+			abundant_numbers.push_back(i);
+}
+
+void create_sums(std::vector<unsigned> &abundant_numbers, bool abund_ind[])
+{
+	// Reset indicators
+	std::memset(abund_ind, false, LIMIT);
+
 	for (unsigned i = 0; i < abundant_numbers.size(); i++) {
 		for (unsigned j = i; j < abundant_numbers.size(); j++) {
 			int tmp = abundant_numbers[i] + abundant_numbers[j];
 			if (tmp <= LIMIT)
-				sums[tmp] = true;
+				abund_ind[tmp] = true;
 			else
 				break;
 		}
@@ -71,5 +107,5 @@ void create_sums(std::vector<unsigned> &abundant_numbers, std::vector<bool> &sum
 
 /*
 	Solution:	4179871
-	Avg time:	0.671717s
+	Avg time:	0.117534s
 */
